c089258de6681435376e516d06e3ce6a3c4bbbf4
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2019-08-29 17:13:51 -0700

    added firebase auth

---
+++ config/Firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCjQwnk2TX2RzifBHbVJhMBp3EWI2wDvtA",
+    authDomain: "not-hotdog-1.firebaseapp.com",
+    databaseURL: "https://not-hotdog-1.firebaseio.com",
+    projectId: "not-hotdog-1",
+    storageBucket: "",
+    messagingSenderId: "895217276432",
+    appId: "1:895217276432:web:99d4c93c65a376a7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
